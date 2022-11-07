3f5049151e31bfd123e92459a4e5b6437f7fc07c
Marketa Polakova <polakovamarketa@email.cz>
2022-11-07 09:50:57 +0100

    homework2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Customers from "./componenets/Customers";
+import ShopCounter from "./componenets/ShopCounter";
+import Clock from "./componenets/Clock";
+import Chart from "./componenets/Chart";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
+  let [goods, setGoods] = useState(0);
+  let [toys, setToys] = useState(0);
+  let [parfume, setParfume] = useState(0);
+  let [electro, setElectro] = useState(0);
+  let numOfCustomers = goods + toys + parfume + electro;
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 1000);
+  }, []);
+
+  const setZero = () => {
+    setGoods(0);
+    setToys(0);
+    setParfume(0);
+    setElectro(0);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        <Col>
+          {" "}
+          <Customers numOfCustomers={numOfCustomers} />
+        </Col>
+        <Col>
+          {" "}
+          <Chart
+            goods={goods}
+            toys={toys}
+            parfume={parfume}
+            electro={electro}
+          />
+        </Col>
+      </Row>
+
+      <Clock dateState={dateState} />
+      <Row>
+        <Col>
+          <ShopCounter
+            setZero={setZero}
+            name="Domácí potřeby"
+            counter={goods}
+            setAddCustomer={setGoods}
+            max={40}
+            dateState={dateState}
+          />
+        </Col>
+        <Col>
+          <ShopCounter
+            setZero={setZero}
+            name="Hračky"
+            counter={toys}
+            setAddCustomer={setToys}
+            max={20}
+            dateState={dateState}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ShopCounter
+            name="Parfémy"
+            setZero={setZero}
+            counter={parfume}
+            setAddCustomer={setParfume}
+            max={10}
+            dateState={dateState}
+          />
+        </Col>
+        <Col>
+          <ShopCounter
+            setZero={setZero}
+            name="Elektro"
+            counter={electro}
+            setAddCustomer={setElectro}
+            max={50}
+            dateState={dateState}
+          />
+        </Col>
+      </Row>
     </div>
   );
 }
